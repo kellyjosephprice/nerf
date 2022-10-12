c8a579f60952d55b73ef173fe6380a1ed44a40f4
Kelly Joseph Price <kellyjosephprice@gmail.com>
2022-10-11 23:31:18 -0700

    darts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useCallback, useState } from 'react';
+
 import './App.css';
 
+import Dart from './Dart';
+
 function App() {
+  const [darts, setDarts] = useState([])
+
+  const fireDart = useCallback(event => {
+    setDarts([...darts, <Dart key={Date.now()} x={event.clientX} y={event.clientY} />])
+  }, [darts])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={fireDart}>
+      {darts}
     </div>
   );
 }
---
+++ src/Dart.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState, useRef } from 'react'
+
+const Dart = ({ x = 0, y = 0 }) => {
+  const [style, setStyle] = useState({})
+  const ref = useRef()
+
+  useEffect(() => {
+    window.requestAnimationFrame(() => {
+      const deg = Math.floor(Math.random() * 360) - 180;
+
+      setStyle({
+        transform: `rotate(${deg}deg)`,
+        top: `${y - 50}px`,
+        left: `${x - 50}px`,
+      })
+    })
+  }, [x, y])
+
+
+  return <div className='Dart_Container' style={style} ref={ref}>
+    <div className='Dart'>
+      <div className='Head' />
+      <div className='Butt' />
+    </div>
+  </div>
+}
+
+export default Dart
