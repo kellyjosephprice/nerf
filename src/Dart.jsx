cd3492b20a897e12cb69152f1b1eaae0e3c05e0e
Kelly Joseph Price <kellyjosephprice@gmail.com>
2022-10-13 22:22:41 -0700

    more accurate distances

--- src/Dart.jsx
+++ src/Dart.jsx
@@ -3,14 +3,21 @@ import { useEffect, useState, useRef } from "react";
 const Dart = ({ x = 0, y = 0 }) => {
   const [style, setStyle] = useState({});
   const ref = useRef();
+  const played = useRef(false)
 
   useEffect(() => {
+    const z = 400;
+    const maxDistance = Math.sqrt((window.innerWidth / 2) ** 2 + window.innerHeight ** 2 + z ** 2)
     const deg = Math.floor(Math.random() * 360) - 180;
     const a = Math.abs(x - window.innerWidth / 2);
     const b = Math.abs(window.innerHeight - y);
-    const duration = 0.4 * (400 + Math.sqrt(a ** 2 + b ** 2));
+    const distance = Math.sqrt(a ** 2 + b ** 2 + z ** 2)
+    const duration = 0.4 * distance;
     const height = 50 + Math.floor(Math.random() * 100);
     let startDeg = (Math.atan(a / b) * 180) / Math.PI;
+    const curveX = 0.5 * (maxDistance - distance) / maxDistance;
+    const curveY = (distance / maxDistance) + 1
+    const curve = `cubic-bezier(${curveX}, ${curveY}, 1, 1)`
 
     if (x < window.innerWidth / 2) {
       startDeg *= -1;
@@ -21,7 +28,7 @@ const Dart = ({ x = 0, y = 0 }) => {
       top: `${y - 50}px`,
       left: `${x - 50}px`,
       height: `${height}px`,
-      transition: `top ${duration}ms cubic-bezier(0,2,1,1), left ${duration}ms ease-out, transform ${duration}ms ease-out, height ${duration}ms cubic-bezier(0,2,1,1)`,
+      transition: `top ${duration}ms ${curve}, left ${duration}ms ease-out, transform ${duration}ms ease-out, height ${duration}ms ${curve}`,
     };
 
     window.requestAnimationFrame(() => {
@@ -33,11 +40,10 @@ const Dart = ({ x = 0, y = 0 }) => {
     });
   }, [x, y]);
 
-  let played = false;
   useEffect(() => {
     const cb = () => {
-      if (played) return;
-      played = true;
+      if (played.current) return;
+      played.current = true;
 
       const nerf = new Audio("/nerf.mp3");
       nerf.volume = 0.1;
